bb961e19b665ef4712fba1536ef0d01b98207242
Massimo Triassi <massimo@plankdesign.com>
2020-10-01 19:07:35 -0400

    init SpringBoot Framework

---
+++ src/main/java/com/soen343/smarthomesimulator/SmartHomeSimulatorApplication.java
@@ -0,0 +1,13 @@
+package com.soen343.smarthomesimulator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartHomeSimulatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartHomeSimulatorApplication.class, args);
+	}
+
+}
