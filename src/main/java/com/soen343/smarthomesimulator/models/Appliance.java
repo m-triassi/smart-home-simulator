c22507bd0e7591eb9881530b992feef44a992589
Massimo Triassi <massimo@plankdesign.com>
2020-10-11 22:13:50 -0400

    Initlaize all models, with services and repositories - minimum class stubs
    add missing dependancies and example application.properties

---
+++ src/main/java/com/soen343/smarthomesimulator/models/Appliance.java
@@ -0,0 +1,80 @@
+package com.soen343.smarthomesimulator.models;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+@Entity
+@Table(name = "appliances")
+public class Appliance {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String type;
+
+    private String name;
+
+    private Integer state;
+
+    @ManyToOne
+    @JoinColumn(name = "home_id")
+    private Home home;
+
+    @ManyToOne
+    @JoinColumn(name = "zone_id")
+    private Zone zone;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getState() {
+        return state;
+    }
+
+    public void setState(Integer state) {
+        this.state = state;
+    }
+
+    public Home getHome() {
+        return home;
+    }
+
+    public void setHome(Home home) {
+        this.home = home;
+    }
+
+    public Zone getZone() {
+        return zone;
+    }
+
+    public void setZone(Zone zone) {
+        this.zone = zone;
+    }
+}
---
+++ src/main/java/com/soen343/smarthomesimulator/models/Opening.java
@@ -0,0 +1,60 @@
+package com.soen343.smarthomesimulator.models;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+
+// Windows, Doors, etc...
+@Entity
+@Table(name = "appliances")
+public class Opening {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String type;
+
+    private Integer state;
+
+    @OneToOne
+    @JoinColumn(name = "zone_id")
+    private Zone zone;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public Integer getState() {
+        return state;
+    }
+
+    public void setState(Integer state) {
+        this.state = state;
+    }
+
+    public Zone getZone() {
+        return zone;
+    }
+
+    public void setZone(Zone zone) {
+        this.zone = zone;
+    }
+}
