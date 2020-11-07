dee6773496eb85dba76285650fe20fa0006858d7
unknown <sustaccosmin544@hotmail.ca>
2020-11-07 18:12:43 -0500

    Add Away toggle button. Changed logged user to a Vuex state rather than a Vuejs prop.

--- src/main/resources/js/components/Profile.vue
+++ src/main/resources/js/components/Profile.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <table class="profile_table" v-if="user.id != undefined">
+    <table class="profile_table" v-if="this.$store.state.user.id != undefined">
       <tr>
         <a href="edit/profile" class="EditButton" :class="{disabled: simulationEnabled}">
           Edit Profile
@@ -10,15 +10,16 @@
         <img class="default_profile_avatar"
              src="https://www.pikpng.com/pngl/m/80-805523_default-avatar-svg-png-icon-free-download-264157.png"
              alt="Default Profile"/>
-        <h5>Signed in user name: {{ user.name }}</h5>
-        <h5>Access type: {{ user.role }}</h5>
+        <h5>Signed in user name: {{ $store.state.user.name }}</h5>
+        <h5>Access type: {{ $store.state.user.role }}</h5>
       </tr>
       <tr>
         <h5>Location:</h5>
-        <h5>{{user.zone.name}}</h5>
+        <h5 v-if="$store.state.user.zone.id != 0">{{$store.state.user.zone.name}}</h5>
+        <h5 v-if="$store.state.user.zone.id == 0">Away</h5>
       </tr>
       <tr>
-        <h5><strong>Outside Temp. {{ this.user.home.outside_temp }}C</strong></h5>
+        <h5><strong>Outside Temp. {{ this.$store.state.user.home.outside_temp }}C</strong></h5>
       </tr>
       <tr>
         <h5><strong>{{ new Date().toLocaleString() }}</strong></h5>
@@ -36,10 +37,12 @@ Vue.component('ToggleButton', ToggleButton)
 
 export default {
   name: 'profile',
-  props: ["user", "simulationEnabled"],
+  props: ["simulationEnabled"],
   data() {
     return {
     };
+  },
+  mounted(){
   }
 }
 
--- src/main/resources/js/components/SecurityModule.vue
+++ src/main/resources/js/components/SecurityModule.vue
@@ -1,22 +1,106 @@
 <template>
-
     <div>
-
-        <p>This is the security module</p>
-
+        <table class="module_table">
+            <tr>
+                <p>This is the security module</p>
+            </tr>
+            <tr>
+                <div>
+                    <toggle-button
+                        :value="this.$store.state.isAway"
+                        :labels="{ checked: 'Away', unchecked: 'At Home' }"
+                        @change="changeState()"
+                        :width="100"
+                        :switch-color="{
+                            checked: '#F2E708',
+                            unchecked: '#08F208',
+                        }"
+                    />
+                </div>
+            </tr>
+        </table>
     </div>
-
 </template>
 
 <script>
-
 export default {
-        name: 'security',
-        data() {
-            return {
-                
-            };
-        }
+    name: 'security',
+    props: ['simulationEnabled'],
+    data() {
+        return {
+            zonesList: {},
+        };
+    },
+    methods: {
+        changeState() {
+            this.$store.commit('changeAwayState');
+            var path = '';
+            if (this.$store.state.isAway) {
+                path =
+                    'user/update?id=' +
+                    this.$store.state.user.id +
+                    '&zone_id=' +
+                    0;
+                this.$store.commit(
+                    'appendMessage',
+                    this.$store.state.user.name +
+                        ' left the house. This user is now [Away]'
+                );
+                this.$store.state.user.zone.id = 0;
+            } else {
+                path =
+                    'user/update?id=' +
+                    this.$store.state.user.id +
+                    '&zone_id=' +
+                    1;
+                this.$store.commit(
+                    'appendMessage',
+                    this.$store.state.user.name +
+                        ' entered the house. This user is now [At Home]'
+                );
+                this.$store.state.user.zone = this.zonesList[0];
+            }
+
+            this.callAxios(path);
+        },
+        callAxios(path) {
+            axios
+                .post(path)
+                .then(function (response) {
+                    console.log(response);
+                })
+                .catch(function (error) {
+                    console.log(error);
+                });
+        },
+        getZones() {
+            var path = 'zones?home_id=' + this.$store.state.user.home.id;
+            axios
+                .get(path)
+                .then((response) => {
+                    this.zonesList = response.data;
+                })
+                .catch(function (error) {
+                    console.log(error);
+                });
+        },
+    },
+    mounted() {
+    setTimeout(this.getZones, 1000);
+  }
+};
+</script>
+
+<style>
+table,
+tr,
+td {
+    border: 1px solid black;
+    border-collapse: collapse;
 }
 
-</script>
+.module_table {
+    width: 50%;
+    height: auto;
+}
+</style>
