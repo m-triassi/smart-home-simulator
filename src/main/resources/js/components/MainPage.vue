1aa50137566bdc68caf19186438d2782866d1312
YordanEvro <yordanpramatarov@yahoo.com>
2020-11-08 21:40:32 -0500

    add icons for openings and lights

--- src/main/resources/js/components/MainPage.vue
+++ src/main/resources/js/components/MainPage.vue
@@ -26,6 +26,14 @@
                     <div v-for="zone in this.$store.state.zones" :key="zone.id" class="zone_box">
                       <p>{{ zone.name }}</p>
                       <p>Users in this zone: {{ zone.users.map(user => { return user.name }).join(", ") }}</p>
+                      <span>Openings in this zone:</span>
+                      <table>
+                          <tr>
+                            <td v-for="(open, index) in zone.openings.map(opening => { return displayOpening(opening) })" :key="index"><span :key="index" v-html="open"></span></td> 
+                          </tr>
+                      </table>
+                      <span>Lights in this zone:</span>
+                      <div v-for="(open, index) in zone.appliances.map(appliance => { return displayLights(appliance) })" :key="index"><span :key="index" v-html="open"></span></div> 
                       <p></p>
                     </div>
                     <p>House Layout</p>
@@ -82,6 +90,7 @@ export default {
                 .get('/zones?home_id=' + this.$store.state.user.home.id)
                 .then((response) => {
                   this.$store.state.zones = response.data;
+                  console.log('514zones ', this.$store.state.zones);
                 })
                 .catch(function (error) {
                     console.log(error);
@@ -126,10 +135,42 @@ export default {
                 }
             }
         },
+        displayOpening(opening){
+            var result;
+
+            if(opening.type == "window"){
+                if(opening.state == 0){
+                    result = "<img class=\"icon\" src=\"https://www.flaticon.com/svg/static/icons/svg/3190/3190083.svg\">"
+                }
+                else{
+                    result = "<img class=\"icon\" src=\"https://www.flaticon.com/svg/static/icons/svg/3190/3190150.svg \">"
+                }
+            }
+            else{
+                if(opening.state == 0){
+                    result = "<img class=\"icon\" src=\"https://www.flaticon.com/svg/static/icons/svg/32/32533.svg\">"
+                }
+                else{
+                    result = "<img class=\"icon\" src=\"https://www.flaticon.com/svg/static/icons/svg/59/59801.svg \">"
+
+                }
+            }
+            return result;
+        },
+        displayLights(appliance){
+            var result;
+            if(appliance.state == 0){
+                result = "<img class=\"icon\" src=\"https://www.flaticon.com/svg/static/icons/svg/82/82648.svg \">"
+            }
+            else{
+                result = "<img class=\"icon\" src=\"https://www.flaticon.com/svg/static/icons/svg/3721/3721106.svg \">"
+            }
+            return result;
+        }
     },
     mounted() {
         this.getUser();
-        this.getZones()
+        setTimeout(this.getZones, 1000);
     },
     data() {
         return {
