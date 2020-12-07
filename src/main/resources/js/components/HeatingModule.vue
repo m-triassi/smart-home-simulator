03ff0c427e5413f746c61a48da6e55ce8c97551e
YordanEvro <yordanpramatarov@yahoo.com>
2020-12-06 22:20:09 -0500

    Add condition to check if zone not null. Fix for heating table to display.

--- src/main/resources/js/components/HeatingModule.vue
+++ src/main/resources/js/components/HeatingModule.vue
@@ -47,7 +47,7 @@
         </td>
         <td>
           <h5 v-if="overwrittenZones[zone.id] == 1">OW</h5>
-          <h5 v-else-if="$store.state.zones[zone.id - 1].overridden == 1">
+          <h5 v-else-if="$store.state.zones[zone.id - 1] && $store.state.zones[zone.id - 1].overridden == 1">
             OW
           </h5>
           <button
@@ -57,7 +57,7 @@
           >
             Restore</button
           ><button
-            v-else-if="$store.state.zones[zone.id - 1].overridden == 1"
+            v-else-if="$store.state.zones[zone.id - 1] && $store.state.zones[zone.id - 1].overridden == 1"
             :id="zone.id"
             v-on:click="restore($event)"
           >
